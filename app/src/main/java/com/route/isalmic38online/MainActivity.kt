231fe3eaad1f264f9a3cd0f3fe95fed56a4eff5f
jawsenhaji <jawadsen2@gmail.com>
2026-06-17 22:28:49 +0100

    تطبيق إسلامي متكامل - النسخة الأولى

---
+++ app/src/main/java/com/route/isalmic38online/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.route.isalmic38online
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
